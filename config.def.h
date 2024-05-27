1ba6dea4fae267889bbc9cf6c67bd5f1af3ed8dd
Azmain Biswas <82164368+AzmainBiswas@users.noreply.github.com.>
2024-05-27 18:54:22 +0530

    change colors to doom one

--- config.def.h
+++ config.def.h
@@ -17,11 +17,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11", "Noto Color Emoji:pixelsize=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#000000";
+static const char col_gray1[]       = "#282c34";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#458588";
+static const char col_gray3[]       = "#bbc2cf";
+static const char col_gray4[]       = "#282c34";
+static const char col_cyan[]        = "#a9a1e1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -37,11 +37,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Gimp",     NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox",  NULL,     NULL,           1 << 8,     0,          0,          -1,        -1 },
+	{ "St",       NULL,     NULL,           0,         0,          1,           0,        -1 },
     { "Alacritty",NULL,     NULL,           0,         0,          1,           0,        -1 },
-    { "kitty",NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "kitty",    NULL,     NULL,           0,         0,          1,           0,        -1 },
     { "org.wezfurlong.wezterm",NULL,     NULL,           0,         0,          1,           0,        -1 },
     { "Pavucontrol", NULL,    NULL,        0,         1,          0,            0,       -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
